37aebae94730b8fa84a49d9f485d674973c76750
Bessie Steinberg <bsteinberg@edx.org>
2019-01-30 14:20:43 -0500

    turn random values into a function

--- src/components/LearnerCompletionsVisualization/index.jsx
+++ src/components/LearnerCompletionsVisualization/index.jsx
@@ -28,7 +28,6 @@ const LearnerCompletionsVisualization = ({
     }}
     showMonthLabels
     showWeekdayLabels
-    horizontal={false}
   />
 );
 
--- src/index.jsx
+++ src/index.jsx
@@ -19,20 +19,35 @@ function getRandomInt(min, max) {
   return Math.floor(Math.random() * ((max - min) + 1)) + min;
 }
 
-function getRange(count) {
-  return Array.from({ length: count }, (_, i) => i);
-}
-
 const today = Date.now();
 
 function App() {
-  const randomValues = getRange(200).map(index => ({
-    date: shiftDate(today, -index),
-    count: getRandomInt(0, 3),
-  }));
+  // const blockTypes = ['html', 'video', 'problem'];
+  function randomValues(numDays) {
+    const values = [];
+    for (let i = numDays; i >= 0; i -= 1) {
+      values.push({
+        date: shiftDate(today, -i),
+        count: getRandomInt(0, 3),
+      });
+    }
+    return values;
+  }
+
+  //
+  //   return getRange(200).map(index => ({
+  //     date: shiftDate(today, -index),
+  //     count: getRandomInt(0, 3),
+  //     completions: getRange(this.count).map({
+  //       block_type: blockTypes[getRandomInt(0, 2)],
+  //       block_key: `block_key_${getRandomInt(0, 99999)}`,
+  //       block_name: `Block Name ${getRandomInt(0, 99999)}`,
+  //     }),
+  //   }));
+  // }
   return (
     <LearnerCompletionsVisualization
-      completionData={randomValues}
+      completionData={randomValues(300)}
     />
   );
 }
